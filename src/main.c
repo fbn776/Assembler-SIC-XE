91836f187825ec49bf00934456f618079a693a09
fbn776 <fbn776@gmail.com>
2024-10-05 01:36:29 +0530

    feat: split by opcode

--- src/main.c
+++ src/main.c
@@ -59,8 +59,6 @@ void assemble_code(Instruction instructions[], int size, Symbol symbols[], int *
         } else if (strcmp(instructions[i].operator, "END") == 0) {
             PGM_LENGTH = LOCCTR - START_ADDR;
             break;
-        } else if() {
-
         }
         // else {
         //     if (strcmp(instructions[i].label, "") != 0) {
@@ -70,6 +68,36 @@ void assemble_code(Instruction instructions[], int size, Symbol symbols[], int *
         //     LOCCTR += 3;
         // }
     }
+}
+
+void create_opcode(char *str, const char *delim, Symbol symtab[], int *size) {
+    int i = 0;
+    char *arr[100];
+    const char *token = strtok(str, delim);
+
+    while (token != NULL && i < 100) {
+        arr[i] = (char *) malloc(strlen(token) + 1);
+        if (arr[i] == NULL) {
+            perror("Failed to allocate memory");
+            exit(EXIT_FAILURE);
+        }
+        strcpy(arr[i++], token);
+        token = strtok(NULL, delim);
+    }
+
+    if (i > 2 || i == 0) {
+        perror("Error: Invalid instruction format\n");
+        exit(1);
+    }
+
+
+    // strcpy(instructions[(*size)].label, i == 1 || i == 2 ? "" : arr[0]);
+    // strcpy(instructions[(*size)].operator, i == 1 || i == 2 ? arr[0] : arr[1]);
+    // strcpy(instructions[(*size)++].operand, i == 1 ? "" : i == 2 ? arr[1] : arr[2]);
+
+    // Free allocated memory
+    for (int j = 0; j < i; j++)
+        free(arr[j]);
 }
 
 int main() {
@@ -86,17 +114,24 @@ int main() {
 
     Instruction instructions[SIZE];
     int size;
-    Symbol symtab[SIZE];
-    int symtab_size = 0;
+
+    Symbol symtab[SIZE], optab[SIZE];
+    int symtab_size = 0, optab_size = 0;
 
     while (getline(&buffer, &bufsize, input_fp) != -1)
         split_string_by_instruct(buffer, " ", instructions, &size);
 
+    create_optab(optab_fp, optab, &optab_size);
     assemble_code(instructions, size, symtab, &symtab_size);
 
+    for(int i = 0; i < optab_size; i++) {
+        printf("%s[%d] = %s\n", optab[i].key, i, optab[i].value);
+    }
 
     // Clean up
     free(buffer);
     fclose(input_fp);
+    fclose(optab_fp);
+
     return 0;
 }
